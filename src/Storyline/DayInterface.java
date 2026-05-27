ac7ead02350a05b0afb8754e04c42f47326bdda3
Memeeme <mylyndeneb.delrosario@cit.edu>
2026-05-28 02:44:57 +0800

    emptied
    wait sa

--- src/Storyline/DayInterface.java
+++
@@ -1,9 +0,0 @@
-package Storyline;
-
-public interface DayInterface {
-    SceneEntry[] getMorningScenes();
-    SceneEntry[] getAfternoonScenes();
-    SceneEntry[] getEveningScenes();
-    ChoiceEntry[] getChoicesForScene(int sceneIndex, String segment);
-    ChoiceEntry[] getChoicesForMarker(String choiceId);
-}
